ba19c6fd184523ffb809133448356357e0ac02a9
ManojRamanan <35764825+ManojRamanan@users.noreply.github.com>
2020-01-16 09:13:19 +0400

    Update JavaDataTypesExample.java

--- JavaDataTypesExample.java
+++ JavaDataTypesExample.java
@@ -22,12 +22,14 @@ class JavaDataTypesExample{
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127){
                     System.out.println("* byte");
-                } if(x>=-32767 && x<=32767){
+                } if(x>=-32768 && x<=32767){
                     System.out.println("* short");
-                } if(x>=-2147483648L && x<=2147483648L){
+                } if(x>=-2147483648 && x<=2147483647L){
                     System.out.println("* int");
                 } if(x>=min && x<= max){
                     System.out.println("* long");
+                }else{
+                    System.out.println(sc.next()+" can't be fitted anywhere.");
                 }
                 //Complete the code
             }
